9db896c0c69499cee0e2bdbf88d941b746543e78
Caio Messias <caio.messias@gerencianet.com.br>
2021-12-18 23:34:05 +0000

    Finaliza rotas para associacoes e desassociacoes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express');
 
 const laboratorioRoutes = require('./laboratorio.routes');
 const exameRoutes = require('./exame.routes');
+const laboratorioExameRoutes = require('./laboratorioExame.routes');
 
 const router = Router();
 
 router.use('/laboratorio', laboratorioRoutes);
 router.use('/exame', exameRoutes);
+router.use('/', laboratorioExameRoutes);
 
 module.exports = router;
