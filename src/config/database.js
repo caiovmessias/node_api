db808a1fa1640dc0f201df7c4d0598d9ba11e85f
Caio Messias <caio.messias@gerencianet.com.br>
2021-12-21 04:19:53 +0000

    Ajusta projeto para usar variavel de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,26 @@
 module.exports = {
-  dialect: 'mysql',
-  host: 'mysql-5.7',
-  username: 'root',
-  password: '12345678',
-  database: 'node_api',
-  seederStorage: 'sequelize'
+  development: {
+    dialect: 'mysql',
+    host: 'mysql-5.7',
+    username: 'root',
+    password: '12345678',
+    database: 'node_api',
+    seederStorage: 'sequelize'
+  },
+  test: {
+    dialect: 'mysql',
+    host: 'mysql-5.7',
+    username: 'root',
+    password: '12345678',
+    database: 'node_api_test',
+    seederStorage: 'sequelize'
+  },
+  production: {
+    dialect: 'mysql',
+    host: 'mysql-5.7',
+    username: 'root',
+    password: '12345678',
+    database: 'node_api',
+    seederStorage: 'sequelize'
+  },
 }
